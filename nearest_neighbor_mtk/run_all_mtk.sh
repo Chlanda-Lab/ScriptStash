d09fd62b8f536cc440625ac24ade7e7bf6febd13
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2024-04-02 20:12:48 +0200

    Add parallelization

--- nearest_neighbor_mtk/run_all_mtk.sh
+++ nearest_neighbor_mtk/run_all_mtk.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 set -e
 
-for modelfile in $(find ./ -maxdepth 2 -iname '*.mod'); do
-    ./mtk.sh" "$modelfile"
+for modelfile in $(find ./models -maxdepth 2 -iname '*.mod'); do
+    ./mtk.sh "$modelfile" &
 done
+wait
